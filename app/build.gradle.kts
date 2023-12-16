62f707d73cfa8b97fb2ed293a645f7e15daf82c0
mukijo-r <mukijo@student.unsia.ac.id>
2023-12-16 09:14:50 +0700

    menambahkan dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     implementation("com.google.android.gms:play-services-cast-framework:21.4.0")
     implementation("com.google.android.engage:engage-core:1.3.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
